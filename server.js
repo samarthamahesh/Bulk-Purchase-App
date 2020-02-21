69b51271687852c0808ed7d829521716006f2a9d
Samartha S M <samarthamahesh@gmail.com>
2020-02-21 17:20:57 +0530

    changed port

--- server.js
+++ server.js
@@ -35,7 +35,7 @@ if(process.env.NODE_ENV === 'production') {
     });
 }
 
-const PORT = process.env.PORT || 5000;
+const PORT = 5000;
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
